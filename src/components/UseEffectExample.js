39d3fe31c4da8e39f346a67608a3ab5769e0577c
caillonantoine <caillon@ircam.fr>
2021-04-01 10:34:03 +0200

    add useEffect

---
+++ src/components/UseEffectExample.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+const UseEffectExample = (props) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(inc, 1000);
+  }, []);
+
+  const inc = () => {
+    setCount((currentCount) => currentCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Current count: {count}</p>{" "}
+    </div>
+  );
+};
+
+// import React from "react";
+// class UseEffectExample extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   componentDidMount() {
+//     setInterval(this.inc.bind(this), 1000);
+//   }
+
+//   inc() {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>Current count: {this.state.count}</p>
+//       </div>
+//     );
+//   }
+// }
+
+export default UseEffectExample;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import UseEffectExample from "./components/UseEffectExample";
 import UseStateExample from "./components/UseStateExample";
 
 // useState example
 // ReactDOM.render(<UseStateExample />, document.getElementById("root"));
+
+// useEffect example
+ReactDOM.render(<UseEffectExample />, document.getElementById("root"));
