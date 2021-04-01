ff3cb59acb43374302822057b95657fbb6a35f15
caillonantoine <caillon@ircam.fr>
2021-04-01 10:36:32 +0200

    add all examples to the same page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import UseEffectExample from "./components/UseEffectExample";
 import UseStateExample from "./components/UseStateExample";
 
 // useState example
-// ReactDOM.render(<UseStateExample />, document.getElementById("root"));
-
-// useEffect example
-ReactDOM.render(<UseEffectExample />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h2>useState example</h2>
+    <UseStateExample />
+    <hr></hr>
+    <h2>useEffect example</h2>
+    <UseEffectExample />
+    <hr></hr>
+  </div>,
+  document.getElementById("root")
+);
